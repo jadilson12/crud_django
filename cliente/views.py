e440345d472885a6bfdee0f5fabdcd3ec0cd49c4
Jadilson Guedes <jadilson12@gmail.com>
2019-04-28 14:50:06 -0300

    view: ultizando lista, for e if no arquivo template

--- cliente/views.py
+++ cliente/views.py
@@ -6,6 +6,9 @@ def home(request):
     # dicionario vario
     data ={}
 
+    # Lista de produtos
+    data['diversos'] = ['Carro', 'motos','avião', 'animais']
+
     # Propriedade do dicionario adionando now
     data['agora'] = datetime.datetime.now()
     return render(request,'cliente/home.html', data)
