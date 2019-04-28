1370edc2822e319b10896d83830394b24a2e597c
Jadilson Guedes <jadilson12@gmail.com>
2019-04-28 14:16:39 -0300

    model: atualizando migrations

--- cliente/admin.py
+++ cliente/admin.py
@@ -2,5 +2,5 @@ from django.contrib import admin
 from .models import *
 # Register your models here.
 admin.site.register(Cliente)
-admin.site.register(Produto)
+admin.site.register(Produt)
 admin.site.register(Categoria)
--- cliente/models.py
+++ cliente/models.py
@@ -6,14 +6,18 @@ class Cliente(models.Model):
     sobrenome = models.CharField(max_length=30)
     dt_criacao = models.DateTimeField(auto_now_add=True)
 
-class Produto(models.Model):
+class Produt(models.Model):
     nome = models.CharField(max_length=15)
     descricao = models.CharField(max_length=100)
     quantidade = models.IntegerField()
     valor = models.DecimalField(max_digits=7, decimal_places=2)
     cliente = models.ForeignKey(Cliente, on_delete=models.CASCADE)
-    observacao = models.TextField()
-    dt_criacao = models.DateTimeField(auto_now_add=True)
+    observacao = models.TextField(null=True, blank=True)
+    data = models.DateTimeField(auto_now=True)
+
+    # renomear a tabela
+    class Meta:
+        verbose_name_plural = 'Produtos'
 
 class Categoria(models.Model):
     nome = models.CharField(max_length=30)
