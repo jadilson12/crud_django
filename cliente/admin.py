17d14a46e42c8bbc8ef25fdf59cc89a653e630af
Jadilson Guedes <jadilson12@gmail.com>
2019-04-28 14:01:32 -0300

    model:adicionou novas class

--- cliente/admin.py
+++ cliente/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
-from .models import Cliente
+from .models import *
 # Register your models here.
 admin.site.register(Cliente)
+admin.site.register(Produto)
+admin.site.register(Categoria)
--- cliente/models.py
+++ cliente/models.py
@@ -5,3 +5,16 @@ class Cliente(models.Model):
     nome = models.CharField(max_length=30)
     sobrenome = models.CharField(max_length=30)
     dt_criacao = models.DateTimeField(auto_now_add=True)
+
+class Produto(models.Model):
+    nome = models.CharField(max_length=15)
+    descricao = models.CharField(max_length=100)
+    quantidade = models.IntegerField()
+    valor = models.DecimalField(max_digits=7, decimal_places=2)
+    cliente = models.ForeignKey(Cliente, on_delete=models.CASCADE)
+    observacao = models.TextField()
+    dt_criacao = models.DateTimeField(auto_now_add=True)
+
+class Categoria(models.Model):
+    nome = models.CharField(max_length=30)
+    dt_criacao = models.DateTimeField(auto_now_add=True)
