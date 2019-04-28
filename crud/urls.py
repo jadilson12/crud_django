2f963d66c99f72ebef4d7106f4006d58a3f79005
Jadilson Guedes <jadilson12@gmail.com>
2019-04-28 15:48:30 -0300

    url: adicionou nova apelido no url e botão na lista de cliente

--- crud/urls.py
+++ crud/urls.py
@@ -21,5 +21,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', home),
     path('clientes/', clientes),
-    path('clientes/novo', novo_cliente),
+    path('clientes/novo', novo_cliente, name='NovoCliente'),
 ]
