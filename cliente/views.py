d9ec9e1f75d884982797f718fdb5135ed7e8371d
Jadilson Guedes <jadilson12@gmail.com>
2019-04-28 13:26:30 -0300

    view: adiciona template para cliente

--- cliente/views.py
+++ cliente/views.py
@@ -1,8 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-import datetime
+
 
 def home(request):
-    now = datetime.datetime.now()
-    html = "<html><body>It is now %s.</body></html>" % now
-    return HttpResponse(html)
+    return render(request,'cliente/home.html')
