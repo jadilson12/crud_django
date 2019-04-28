8b22aa4710fd89d7fc3c38a65edd8f307eb2124c
Jadilson Guedes <jadilson12@gmail.com>
2019-04-28 14:35:08 -0300

    view: retornando uma variavel para view

--- cliente/views.py
+++ cliente/views.py
@@ -1,5 +1,11 @@
 from django.shortcuts import render
-
+import datetime
 
 def home(request):
-    return render(request,'cliente/home.html')
+
+    # dicionario vario
+    data ={}
+
+    # Propriedade do dicionario adionando now
+    data['agora'] = datetime.datetime.now()
+    return render(request,'cliente/home.html', data)
