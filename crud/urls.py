85444c6ae199591c5fb5ba6ffa695dc84a437118
Jadilson Guedes <jadilson12@gmail.com>
2019-04-29 17:07:07 -0300

    urls: Ativando a visualização direto pelo link na view admin

--- crud/urls.py
+++ crud/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from cliente.views import *
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -24,4 +26,4 @@ urlpatterns = [
     path('clientes/novo', novo_cliente, name='NovoCliente'),
     path('clientes/update/<int:pk>', update_cliente, name='UpdateCliente'),
     path('clientes/delete/<int:pk>', delete_cliente, name='DeleteCliente'),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
