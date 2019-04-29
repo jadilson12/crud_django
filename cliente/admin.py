764a6ea95c541247a4316cfe34cbd60f15b93f8c
Jadilson Guedes <jadilson12@gmail.com>
2019-04-29 14:30:47 -0300

    admin: customizando a view admin cliente com list_display

--- cliente/admin.py
+++ cliente/admin.py
@@ -1,8 +1,18 @@
 from django.contrib import admin
 from .models import *
-# Register your models here.
-admin.site.register(Cliente)
+
+
+class ClienteAdmin(admin.ModelAdmin):
+    list_display = ('nome','sobrenome', 'endereco', 'email', 'idade', 'cpf')
+
+
+class TelefoneAdmin(admin.ModelAdmin):
+    list_display = ('telefone', 'descricao', 'cliente')
+
+
+admin.site.register(Cliente, ClienteAdmin)
 admin.site.register(Produt)
 admin.site.register(Categoria)
-admin.site.register(Telefone)
+admin.site.register(Telefone, TelefoneAdmin)
 admin.site.register(Cpf)
+admin.site.register(Grupo)
