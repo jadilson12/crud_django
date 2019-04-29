fda6e4b4ce5e1924d1ab01cdf627108860e7a703
Jadilson Guedes <jadilson12@gmail.com>
2019-04-29 12:40:05 -0300

    model: adicionou class telefone

--- cliente/models.py
+++ cliente/models.py
@@ -1,14 +1,23 @@
 from django.db import models
 
+class Telefone(models.Model):
+    numero = models.CharField(max_length=20)
+    descricao = models.CharField(max_length=100)
+
 
 class Cliente(models.Model):
     nome = models.CharField(max_length=30)
     sobrenome = models.CharField(max_length=30)
+    endereco = models.CharField(max_length=200, blank=False, null=False)
+    salario = models.DecimalField(max_digits=10, decimal_places=2)
+    idade = models.IntegerField()
+    email = models.EmailField()
     dt_criacao = models.DateTimeField()
 
     # Converte objeto para o nome de eximição na  view
     def __str__(self):
         return self.nome
+
 
 class Produt(models.Model):
     nome = models.CharField(max_length=15)
@@ -17,15 +26,17 @@ class Produt(models.Model):
     valor = models.DecimalField(max_digits=7, decimal_places=2)
     cliente = models.ForeignKey(Cliente, on_delete=models.CASCADE)
     observacao = models.TextField(null=True, blank=True)
-    data = models.DateTimeField() # Exibe o calendário e horario na view
+    data = models.DateTimeField()  # Exibe o calendário e horario na view
 
-    # renomear a tabela
+    # rename a table
+
     class Meta:
         verbose_name_plural = 'Produtos'
 
-    # Converte objeto para o nome de eximição na  view
+    # Converte object para o nome de eximição na  view
     def __str__(self):
         return self.nome
+
 
 class Categoria(models.Model):
     nome = models.CharField(max_length=30)
